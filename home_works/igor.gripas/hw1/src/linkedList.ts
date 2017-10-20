de44d4638f0cb8cb6fe89e806faf2b075f46fd2e
igor <igor.gripas@ardas.dp.ua>
2017-10-20 18:10:37 +0300

    igor hw1

--- home_works/igor.gripas/hw1/src/linkedList.ts
+++ home_works/igor.gripas/hw1/src/linkedList.ts
@@ -3,7 +3,14 @@ class LinkedList<T> {
 
     public add(value:T):void {
         if (null === this.element) {
-            element = new ListElement(value, )
+            this.element = new ListElement(value);
+            this.element.prev = this.element;
+            this.element.next = this.element;
+        } else {
+            const newElement = new ListElement(value);
+            newElement.prev = this.element;
+            newElement.next = this.element.next;
+            this.element = newElement;
         }
     }
 }
@@ -13,9 +20,17 @@ class ListElement<T> {
     public next: ListElement<T>;
     public prev: ListElement<T>;
 
-    constructor(value: T, next:ListElement<T>, prev:ListElement<T>) {
+    constructor(value: T) {
         this.value = value;
-        this.next = next;
-        this.prev = prev;
     }
 }
+
+const list = new LinkedList();
+list.add('first');
+list.add('second');
+list.add('third');
+
+console.log('current', list.element.value);
+console.log('prev', list.element.prev.value);
+console.log('prev prev', list.element.prev.prev.value);
+console.log('next', list.element.next.value);
